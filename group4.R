51a33707d0824a0b7ad6af02bd71bbd0eeefb2c8
Wenqi-528 <60374477+Wenqi-528@users.noreply.github.com>
2020-10-08 11:58:40 -0400

    Add files via upload

---
+++ group4.R
@@ -0,0 +1,42 @@
+#setwd('set your own')
+library(psych)
+rm(list=ls())
+#Set your own group4.dat location
+data<- read.table("/Users/wenqizhou/Desktop/2020 Fall/EDUC 684 /c6/Group 4.dat", header=TRUE)
+data
+
+strsplit(data[1,1], "|")
+
+data[1,1]
+
+x<-vector()
+y<-vector()
+for (i in (1:11) )
+    { x<-cbind(x,unlist(strsplit(data[i,1], "[|]"))[1])
+     y<-cbind(y,unlist(strsplit(data[i,1], "[|]"))[2])}
+
+attributes(x)
+
+data<-data.frame(cbind(t(x),t(y)))
+data
+x<-as.numeric(data$X1)
+y<-as.numeric(data$X2)
+x
+y
+
+
+describe(x)
+describe(y)
+cor(x,y)
+
+
+reg<-lm(y~x)
+summary(reg)
+summary(reg)$r.squared
+plot(x,y, xlab="X",
+     ylab="Y", 
+     main = "y is regressed on x")
+abline(coef(reg))
+#p-values of t-test are below the 0.05 threshold, so the model is indeed statistically significant.
+#linear model fit explains a 66.67% of the variance observed in the data.
+#p-values of f-test are below the 0.05 threshold
